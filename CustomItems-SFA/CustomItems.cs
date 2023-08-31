df83a62947173cf1346b38a8cbaf513364d55391
KadotCom (Kaiden) <76606192+kadotcom@users.noreply.github.com>
2023-08-30 20:23:49 -0400

    Init

---
+++ CustomItems-SFA/CustomItems.cs
@@ -0,0 +1,59 @@
+// -----------------------------------------------------------------------
+// <copyright file="CustomItems.cs" company="Joker119">
+// Copyright (c) Joker119. All rights reserved.
+// Licensed under the CC BY-SA 3.0 license.
+// </copyright>
+// -----------------------------------------------------------------------
+
+#pragma warning disable SA1200
+namespace CustomItems;
+
+using System;
+using Events;
+using Exiled.API.Features;
+using Exiled.CustomItems.API.Features;
+using HarmonyLib;
+using Server = Exiled.Events.Handlers.Server;
+
+/// <inheritdoc />
+public class CustomItems : Plugin<Config>
+{
+    private ServerHandler serverHandler = null!;
+
+    /// <summary>
+    /// Gets the Plugin instance.
+    /// </summary>
+    public static CustomItems Instance { get; private set; } = null!;
+
+    public override string Name { get; } = "CustomItems";
+    public override string Author { get; } = "Joker119 and the Exiled Team | Modified by KadotCom and SiteFunnyArea";
+    public override string Prefix { get; } = "sfa_custom_items";
+    public override Version Version { get; } = new(1,0,0,0);
+    /// <inheritdoc/>
+    public override Version RequiredExiledVersion { get; } = new(7, 2, 0);
+
+    /// <inheritdoc/>
+    public override void OnEnabled()
+    {
+        Instance = this;
+        serverHandler = new ServerHandler();
+
+        Config.LoadItems();
+
+        Log.Debug("Registering items..");
+        CustomItem.RegisterItems(overrideClass: Config.ItemConfigs);
+        Server.ReloadedConfigs += serverHandler.OnReloadingConfigs;
+
+        base.OnEnabled();
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisabled()
+    {
+        CustomItem.UnregisterItems();
+
+        Server.ReloadedConfigs -= serverHandler.OnReloadingConfigs;
+
+        base.OnDisabled();
+    }
+}
